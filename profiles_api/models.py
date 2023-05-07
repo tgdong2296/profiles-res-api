c67e62bd7745e66b0d1ea6ebcd3608557ea11f48
Developer <>
2023-05-07 12:20:55 +0700

    [Bug] Create super user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,10 +22,10 @@ class UserProfileManager(BaseUserManager):
     
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
-        user = self.create_user(email, name, password)
+        user = self.create_user(name, email, password)
 
         user.is_superuser = True
-        user.is_staff = False
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
